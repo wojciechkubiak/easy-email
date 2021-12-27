6ca9666511185480539d12e51f146fe7a934567c
Wojciech Kubiak <w.kubiak@xberry.tech>
2021-12-27 14:28:55 +0100

    Working email sender

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 3000;
+
+const sendEmail = async ({ name, email, message }) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: `"${name}" <${email}>`,
+        to: process.env.EMAIL_RECEIVER,
+        subject: `CopySearcher Website Message - ${name}`,
+        text: `${message}`,
+        html: `<h3>New message from ${email}</h3>
+            <p>${message}</p>
+        `,
+        replyTo: `${email}`,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    sendEmail({ name: req.body?.name, email: req.body?.email, message: req.body?.message }).catch(console.error);
+
+    res.send('Email sent');
+})
+
+app.listen(port, () => {
+    console.log(`App listening at PORT: ${port}`)
+})
